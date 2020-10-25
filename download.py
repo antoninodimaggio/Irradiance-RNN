a9356c6d638abebc1eedee1894bc6ef3042391ce
antoninodimaggio <antoninojdimaggio@gmail.com>
2020-10-25 13:30:34 -0400

    Add multi-year download.py

--- download.py
+++ download.py
@@ -1,37 +1,64 @@
 import argparse
 import json
+import os
 import numpy as np
 import pandas as pd
 
 
-def download_data(lat, lon, year, leap_year, interval, attributes='ghi',
-                  utc='false', mailing_list='false'):
+def define_paths(lat, lon, year):
+    dir_path = f'./data/csv/{lat}_{lon}/'
+    make_dir(dir_path)
+    return f'{dir_path}/{year}.csv'
+
+
+def make_dir(dir_path):
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
+
+
+def format_url(lat, lon, year, leap_year, interval, attributes='ghi', utc='false',
+    mailing_list='false'):
     with open('./config/config.json') as json_file:
         data = json.load(json_file)
-    api_key = data['API_KEY']
-    your_name = data['YOUR_NAME']
-    your_affiliation = data['YOUR_AFFILIATION']
-    reason_for_use = data['REASON_FOR_USE']
-    your_email = data['YOUR_EMAIL']
-    url_frmt_str = ('https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv'
-                    '?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval'
-                    '={interval}&utc={utc}&full_name={name}&email={email}&affiliation'
-                    '={affiliation}&mailing_list={mailing_list}&reason={reason}'
-                    '&api_key={api}&attributes={attr}')
-    url = url_frmt_str.format(year=year, lat=lat, lon=lon, leap=leap_year,
-                              interval=interval, utc=utc, name=your_name, email=your_email,
-                              mailing_list=mailing_list, affiliation=your_affiliation,
-                              reason=reason_for_use, api=api_key, attr=attributes)
+    url_frmt_str =  (
+        f'https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv'
+        f'?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap_year}&interval'
+        f'={interval}&utc={utc}&full_name={data["YOUR_NAME"]}'
+        f'&email={data["YOUR_EMAIL"]}&affiliation={data["YOUR_AFFILIATION"]}'
+        f'&mailing_list={mailing_list}&reason={data["REASON_FOR_USE"]}'
+        f'&api_key={data["API_KEY"]}&attributes={attributes}'
+    )
+    return url_frmt_str
+
+
+def split_years(years):
+    """years should be a comma seperated string"""
+    return years.split(',')
+
+
+def download(lat, lon, year, leap_year, interval):
+    """https://developer.nrel.gov/docs/solar/nsrdb/psm3-download/"""
     print('Start downloading ...')
+    url = format_url(lat, lon, year, leap_year, interval)
     df = pd.read_csv(url, skiprows=2).dropna(axis=1)
     df.insert(0, 'Date', pd.to_datetime(df.loc[:, ['Year', 'Month', 'Day', 'Hour', 'Minute']],
               format= '%Y-%m-%d %H:%M'))
     df.drop(['Year', 'Month', 'Day', 'Hour', 'Minute'], axis=1, inplace=True)
     df = df[df.loc[:, 'GHI'] > 0]
-    csv_path = f'./data/csv/{lat}_{lon}_{year}.csv'
+    csv_path = define_paths(lat, lon, year)
     print(f'Saving csv to the path: {csv_path}')
     df.to_csv(csv_path, index=False)
     print('Done downloading!')
+    return csv_path
+
+
+def download_all(lat, lon, years, leap_year, interval):
+    df = pd.DataFrame(columns=['Date','GHI'])
+    years = split_years(years)
+    for year in years:
+        csv_path = download(lat, lon, year, leap_year, interval)
+        df = df.append(pd.read_csv(csv_path, parse_dates=True))
+    df.to_csv(f'./data/csv/{lat}_{lon}/{"_".join(years)}.csv', index=False)
 
 
 def main():
@@ -42,7 +69,7 @@ def main():
     parser.add_argument('--lon', type=float,
         help='Longitude (to avoid errors make sure this value is within the continental United States)',
         required=True)
-    parser.add_argument('--year', type=int, help='Year (1998-2017 according to the official NSRDB docs)', required=True)
+    parser.add_argument('--year', type=str, help='Year (1998-2017 according to the official NSRDB docs)', required=True)
     parser.add_argument('--leap-year', type=str, default='false',
         help='Is it a leap year, make sure the string is either true or false (default: false)')
     parser.add_argument('--interval', type=str, default='30', help='30 or 60 minute interval data (default: 30)')
@@ -54,8 +81,19 @@ def main():
         parser.print_help()
         raise ValueError('--leap-year should either be true or false make sure everything is lowercase')
 
-    download_data(args.lat, args.lon, args.year, args.leap_year, args.interval)
+    download_all(args.lat, args.lon, args.year, args.leap_year, args.interval)
 
 
 if __name__ == '__main__':
+    """
+    Notes:
+        lat comes before lon
+    """
+    """
+    python download.py --lat 33.2164 \
+      --lon -97.1292 \
+      --year 2010,2011 \
+      --leap-year false \
+      --interval 30
+    """
     main()
