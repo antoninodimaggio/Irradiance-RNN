f7b843f8f375097e278f395dec31eeb429e1e83f
antoninodimaggio <antoninojdimaggio@gmail.com>
2020-10-27 13:32:09 -0400

    Refactor and Reformat

--- train.py
+++ train.py
@@ -1,26 +1,103 @@
 import argparse
-from rnn.train_evaluate_plot import train
+
+from irradiance_rnn.train import train
+
 
 def main():
-    # long lines
     parser = argparse.ArgumentParser(description='Train a configurable RNN')
-    parser.add_argument('--train-data-path', type=str, help='Path to training data [Required]', required=True)
-    parser.add_argument('--test-data-path', type=str, help='Path to testing data [Required]', required=True)
-    parser.add_argument('--name', type=str, default='model', help='Name of the saved model (default: model)')
-    parser.add_argument('--batch-size', type=int, default=64, help='Batch size of the training data (default: 64)')
-    parser.add_argument('--seq-length', type=int, default=64, help='How many data points are needed to make one prediction (default: 64)')
-    parser.add_argument('--hidden-size', type=int, default=35, help='How many hidden neurons per LSTM layer (default: 35)')
-    parser.add_argument('--num-layers', type=int, default=2, help='How many LSTM layers (default: 2)')
-    parser.add_argument('--dropout', type=float, default=0.3, help='Dropout rate (default: 0.3)')
-    parser.add_argument('--epochs', type=int, default=8, help='Number of epochs (default: 8)')
-    parser.add_argument('--lr', type=float, default=1e-2, help='Beginning learning rate (default: 1e-2)')
-    parser.add_argument('--decay', type=float, default=1e-5, help='Weight decay also known as L2 penalty (default: 1e-5)')
-    parser.add_argument('--step-size', type=int, default=2, help='Decays the learning rate of each parameter group by gamma every step_size epochs (default: 2)')
-    parser.add_argument('--gamma', type=float, default=0.5, help='Multiplicative factor of learning rate decay (default: 0.5)')
-    args = parser.parse_args()
-    train(args.train_data_path, args.test_data_path, args.name, args.batch_size,
-          args.seq_length, args.hidden_size, args.num_layers, args.dropout,
-          args.epochs, args.lr, args.decay, args.step_size, args.gamma)
+    parser.add_argument(
+        '--lat', type=float, required=True, help='Latitude [Required]'
+    )
+    parser.add_argument(
+        '--lon', type=float, required=True, help='Longitude [Required]'
+    )
+    parser.add_argument(
+        '--train-years',
+        type=str,
+        required=True,
+        help=
+        'Comma seperated value string of downloaded irradaince data [Required]'
+    )
+    parser.add_argument(
+        '--seq-length',
+        type=int,
+        default=64,
+        help=
+        'How many data points are needed to make one prediction [default: 64]'
+    )
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        default=64,
+        help='Batch size of the training data [default: 64]'
+    )
+    parser.add_argument(
+        '--model-name',
+        type=str,
+        default='model',
+        help='Name of the saved model [default: model]'
+    )
+    parser.add_argument(
+        '--start-date',
+        type=str,
+        default=None,
+        help='Start date if you want to slice [default: None]'
+    )
+    parser.add_argument(
+        '--end-date',
+        type=str,
+        default=None,
+        help='End date if you want to slice [default: None]'
+    )
+    parser.add_argument(
+        '--hidden-size',
+        type=int,
+        default=35,
+        help='How many hidden neurons per LSTM layer [default: 35]'
+    )
+    parser.add_argument(
+        '--num-layers',
+        type=int,
+        default=2,
+        help='How many LSTM layers [default: 2]'
+    )
+    parser.add_argument(
+        '--dropout',
+        type=float,
+        default=0.3,
+        help='Dropout rate [default: 0.3]'
+    )
+    parser.add_argument(
+        '--epochs', type=int, default=5, help='Number of epochs [default: 5]'
+    )
+    parser.add_argument(
+        '--lr',
+        type=float,
+        default=1e-2,
+        help='Beginning learning rate [default: 1e-2]'
+    )
+    parser.add_argument(
+        '--decay',
+        type=float,
+        default=1e-5,
+        help='Weight decay also known as L2 penalty [default: 1e-5]'
+    )
+    parser.add_argument(
+        '--step-size',
+        type=int,
+        default=2,
+        help=
+        'Decays the learning rate of each parameter group by gamma every step_size epochs [default: 2]'
+    )
+    parser.add_argument(
+        '--gamma',
+        type=float,
+        default=0.5,
+        help='Multiplicative factor of learning rate decay [default: 0.5]'
+    )
+    args = vars(parser.parse_args())
+    train(**args)
+
 
 if __name__ == '__main__':
     main()
